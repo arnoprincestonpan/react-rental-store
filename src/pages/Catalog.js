ca57b739f3e49165d7e331c6e24312f47effbc78
ARNO PAN <apan11@my.bcit.ca>
2023-05-01 12:48:12 -0700

    catalog table added

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -9,10 +9,43 @@
  */
 
 import React from 'react'
+import catalogData from "../data/catalog.json"
 
 function Catalog() {
+  const catalogItems = catalogData.map(catalog => 
+    (
+      <tr key={catalog.titleId}>
+        <td>{catalog.titleId}</td>
+        <td>{catalog.title}</td>
+        <td>{catalog.itemCopies.length}</td>
+        <td>{catalog.genre}</td>
+        <td>
+          { 
+            [...new Set(catalog.itemCopies.map(item => item.itemMedia))].join(", ")
+          }
+        </td>
+      </tr>
+    )
+  )
   return (
-    <div>Catalog</div>
+    <div className="catalog">
+      <h1>Catalog</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title Id</th>
+            <th>Title</th>
+            <th>Number of Copies</th>
+            <th>Date Released</th>
+            <th>Genre</th>
+            <th>Types of Media</th>
+          </tr>
+        </thead>
+        <tbody>
+          {catalogItems}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
