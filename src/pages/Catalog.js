7fe3d651036d6be5bf341b09db6df8dcfc657288
ARNO PAN <apan11@my.bcit.ca>
2023-05-01 23:15:48 -0700

    updated routes

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -5,48 +5,64 @@
  * - By Media Type
  * - By Genre Type
  * - By Year (or date)
- * - 
+ * -
  */
 
-import React from 'react'
-import catalogData from "../data/catalog.json"
+import React from "react";
+import catalogData from "../data/catalog.json";
+import {Link, useMatch} from "react-router-dom"
 
 function Catalog() {
-  const catalogItems = catalogData.map(catalog => 
-    (
+  const catalogItems = catalogData.map((catalog) => {
+    // Count the number of occurrences of each media type
+    const mediaCounts = catalog.itemCopies.reduce((counts, itemCopy) => {
+      const media = itemCopy.itemMedia;
+      counts[media] = (counts[media] || 0) + 1;
+      return counts;
+    }, {});
+
+    // Convert the mediaCounts object into an array of key-value pairs
+    const mediaCountsArray = Object.entries(mediaCounts);
+
+    return (
       <tr key={catalog.titleId}>
         <td>{catalog.titleId}</td>
         <td>{catalog.title}</td>
         <td>{catalog.itemCopies.length}</td>
+        <td>{catalog.dateReleased}</td>
         <td>{catalog.genre}</td>
         <td>
-          { 
-            [...new Set(catalog.itemCopies.map(item => item.itemMedia))].join(", ")
-          }
+          {mediaCountsArray
+            .map(([media, count]) => `${media} (${count})`)
+            .join(", ")}
+        </td>
+        <td>
+          <Link to={`/catalog/${catalog.titleId}`}>View</Link>
+          {' | '}
+          <Link to={`/catalog/${catalog.titleId}/edit`}>Edit</Link>
         </td>
       </tr>
-    )
-  )
+    );
+  });
   return (
     <div className="catalog">
       <h1>Catalog</h1>
       <table>
         <thead>
           <tr>
-            <th>Title Id</th>
+            <th>Id</th>
             <th>Title</th>
             <th>Number of Copies</th>
             <th>Date Released</th>
             <th>Genre</th>
             <th>Types of Media</th>
+            <th>Action</th>
           </tr>
         </thead>
-        <tbody>
-          {catalogItems}
-        </tbody>
+        <tbody>{catalogItems}</tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default Catalog
+export default Catalog;
