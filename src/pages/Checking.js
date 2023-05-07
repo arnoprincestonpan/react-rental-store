b517c7a7c05ad74f53b4437fdb6eb75b27c3eb97
Arno Princeston Pan <apan11@my.bcit.ca>
2023-05-07 02:54:03 -0700

    Need to disable login button when quicklogincode is empty

--- src/pages/Checking.js
+++ src/pages/Checking.js
@@ -8,18 +8,35 @@ function Checking() {
     const [catalog, setCatalog] = useState(catalogData)
     const [customers, setCustomers] = useState(customerData)
     const [staff, setStaff] = useState(staffData)
+
     const [staffLogged, setStaffLogged] = useState("Guest Staff")
+    const [staffPassword, setStaffPassword] = useState("")
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    const handleLogIn = (quickLoginCode) => {
+        const staffIndex = staff.findIndex(staff => staff.quickLogin === quickLoginCode)
+        if(staffIndex !== -1){
+            setStaffLogged(`${staff[staffIndex].firstName} ${staff[staffIndex].lastName}`)
+            setLoggedIn(true)
+        }
+    }
 
     return (
     <div className='container'>
         <h1>Checking</h1>
-        <form>
-            <div class="form-group">
-                <label for="staffQuickLogIn">Staff Quick Login</label>
-                <input type="password" class="form-control" id="staffQuickLogin" placeholder="Enter Quick Login Code"/>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        <div className="mb-1">
+            <form className="d-flex flex-row">
+                <div className="form-group">
+                    <div>
+                        <label for="staffQuickLogIn"><p className="text-uppercase">Staff Quick Login</p></label>
+                    </div>
+                    <div className="d-flex flex-row mx-auto">
+                        <input type="password" className="form-control" id="staffQuickLogin" onChange={(e) => setStaffPassword(e.targetValue)} placeholder="Enter Quick Login Code"/>
+                        <button type="submit" className="btn btn-primary border mx-1" onClick={handleLogIn(staffPassword)} disabled={!staffPassword}>Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
     )
 }
