a1a875c84b523502e0a6ca9fa7e737f7a87ba929
ARNO PAN <apan11@my.bcit.ca>
2023-05-12 16:20:35 -0700

    input to password

--- src/pages/Checking.js
+++ src/pages/Checking.js
@@ -4,9 +4,10 @@ import customerData from "../data/customer.json"
 import staffData from "../data/staff.json"
 
 function Checking() {
+    const defaultLogIn = "Guest Staff"
     const [staffInfo, setStaffInfo] = useState(staffData)
     const [shortPassword, setShortPassword] = useState("")
-    const [staffLoggedIn, setStaffLoggedIn] = useState("Not Logged In")
+    const [staffLoggedIn, setStaffLoggedIn] = useState(defaultLogIn)
 
     const handlePasswordSubmit = () => {
         console.log(shortPassword)
@@ -15,13 +16,12 @@ function Checking() {
         console.log(staffInfoIndex)
         if(staffInfo !== -1){
             setStaffLoggedIn(`${staffInfo[staffInfoIndex].firstName} ${staffInfo[staffInfoIndex].lastName}`)
-        }
+        } 
     }
 
     return (
     <div className='container'>
-        <p>Staff Logged In: <b>{staffLoggedIn}</b></p>
-        <input className="border m-1" type="text" id="shortPassword" name="shortPassword" onChange={(e) => setShortPassword(e.target.value)}/>
+        <input className="border m-1" type="password" id="shortPassword" name="shortPassword" onChange={(e) => setShortPassword(e.target.value)} value={shortPassword}/>
         <button className="btn btn-primary border m-1" onClick={() => handlePasswordSubmit()}>Quick Login</button>
     </div>
     )
